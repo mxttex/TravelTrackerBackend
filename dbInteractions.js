fdeddbbc275de463850343271984a7c11b29a04b
Matteo Faccetta <matteofaccetta@gmail.com>
2025-05-14 08:45:19 +0200

    aggiornato scheletro

--- dbInteractions.js
+++ dbInteractions.js
@@ -1,36 +1,66 @@
 const mariadb = require('mariadb')
 const config = require('connection.js')
 
-async function AddUser(data)  {
+async function AddUser(data) {
     try {
-        let connection = mariadb.createConnection(config)
-        return new Promise(
-            (resolve) => {
-                setTimeout(function () {
-                    resolve(
-                        connection.query(
-                            `INSERT INTO TravelTrackerDB.Cliente(Username, Mail, Nome, Cognome, Password, DataDiNascita)
-                             VALUES
-                             (?,?,?,?,?,?)`,
-                             [data.Username, data.Mail, data.Nome, data.Cognome, data.Password, data.DataDiNascita]
-                        )
-                    )
-                }, 2000)
-            })
+        return DoQuery([data.Username, data.Mail, data.Nome, data.Cognome, data.Password, data.DataDiNascita],
+                       `INSERT INTO TravelTrackerDB.Cliente(Username, Mail, Nome, Cognome, Password, DataDiNascita)
+                        VALUES
+                        (?,?,?,?,?,?)`)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+async function TryToLog(data){
+    try {
+        return DoQuery(data, `SELECT Username, Mail, Nome, Cognome, DataDiNascita FROM Clienti WHERE Username=? AND Password=?`)
     } 
     catch (error) {
         throw new Error(error)
     }
 }
 
-async function TryToLog(body){
+async function AddViaggio(data){
     try {
-        return DoQuery(data)
-    } 
-    catch (error) {
-        throw new Error(error)
+        
+    } catch (error) {
+        
     }
 }
+
+async function AddTratta(data){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+async function FetchAllViaggiGivenUser(data){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+async function FetchTratteGivenViaggio(data){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+async function FetchViaggioGivenPartenzaArrivoUtente(data){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
 
 const DoQuery = async (data, query) => {
     let connection = mariadb.createConnection(config)
@@ -46,4 +76,9 @@ const DoQuery = async (data, query) => {
                 })
             }
         )
+}
+
+module.exports = {
+    TryToLog: TryToLog,
+    AddUser : AddUser
 }
